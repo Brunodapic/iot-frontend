45c7613693daef4f7e4e9e8d75886e0a0a9d45f4
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2023-05-29 20:47:50 +0200

    merge

--- src/pages/actuate/Actuate.jsx
+++ src/pages/actuate/Actuate.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import mqtt from "precompiled-mqtt";
+import { Button, Typography } from "@mui/material";
+
+//const URL = " wss://161.53.19.19:56183/";
+const URL = "wss://test.mosquitto.org:8081/";
 
 const Actuate = () => {
-  return <div>hahaha</div>;
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    console.log("CONNECTing");
+    const cli = mqtt.connect(URL);
+    cli.on("connect", () => {
+      console.log("CONNECTED to broker");
+      setClient(cli);
+    });
+  }, []);
+
+  function handleActuationClick() {
+    console.log("client ", client);
+    client.publish("grupa23", {
+      led: "enabled",
+    });
+  }
+
+  return (
+    <div className="mt-20 grid">
+      <Typography variant="h4" gutterBottom>
+        If you want to trigger actuation, click on the button bellow!
+      </Typography>
+      <div className="flex justify-center mt-4">
+        <Button size="large" variant="contained" onClick={handleActuationClick}>
+          Actuate
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Actuate;
