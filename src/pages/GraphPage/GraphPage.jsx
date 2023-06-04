69c85ddd403580a384508ec923372c64facaaced
Brunodapic <44134722+Brunodapic@users.noreply.github.com>
2023-06-04 11:18:25 +0200

    Solids

--- src/pages/GraphPage/GraphPage.jsx
+++ src/pages/GraphPage/GraphPage.jsx
@@ -9,6 +9,7 @@ function GraphPage() {
     { name: "Temperature", url: "G23Temp" },
     { name: "Ph", url: "G23ph" },
     { name: "Humidity", url: "G23Hum" },
+    { name: "Solids", url: "G23Solids" },
   ];
 
   return (
@@ -17,6 +18,8 @@ function GraphPage() {
         <button onClick={()=>setPage(0)}>Temperature</button>
         <button onClick={()=>setPage(1)} >Ph</button>
         <button onClick={()=>setPage(2)} >Humidity</button>
+        <button onClick={()=>setPage(3)} >Solids</button>
+
       </div>
       <DataComponent data={pages[page]} />
     </div>
